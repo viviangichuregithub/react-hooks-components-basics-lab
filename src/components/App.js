ad247dd3816d2bee11d9e890b38074b7c6db3c64
Vivian Gichure <viviangichure@gmail.com>
2025-07-09 09:27:28 +0300

    completed lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 function NavBar() {
   return (
     <nav>
@@ -16,15 +17,23 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
+
+function About() {
+  return (
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  );
+}
+
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
